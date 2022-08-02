9c61bf9366a29279fb88fbfe29b10c35d6037d35
PonmuraliGIT <murali81rmspj@gmail.com>
2022-08-02 21:20:23 +0530

    First commit
    First commit

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Thanks for the supporters
+
+I would like to thank the below creators of the reference,support staffs from ineuron and hitesh sir,Anurag sir
+
+- [W3Schools](https://www.w3schools.com/)
+- [Clippath](https://bennettfeely.com/clippy/)
+- [Pexels](https://pexels.com)
+- [Unsplash](https://unsplash.com/)
+- [Google](https://google.com)
+- [Flexbox](https://flexboxfroggy.com/)
+- [HTML CSS Learning](https://learn.shayhowe.com/)
+- [iNeuron](https://ineuron.ai/)
+- [LCO](https://courses.learncodeonline.in/learn)
+- [iconscout](https://iconscout.com/)
+
+  # Project Features
+
+1. I have created this project with pure HTML and CSS with some flavours of JS and Jquery
+
+1. I have made this website responsive.
+
+1. Sticky Navbar and smooth scrolling effects done. - Create a figma file for tech avengers image.
+1. Created menu for both desktop as well as responsive.
+1. Top scrolling is enabled.
+1. In Hero section if we hover the light image another image will be displayed.
+1. Buttons has small animation gradient effect.
+1. got some images from ineuron website.
+1. Hover icons from Our products section for
+1. smooth transition of icon images.
+1. Created two themes for the website and included the link in Theme menu.
+
+## Additional Information
+
+- Ponmurali P J Freelancer, Madurai , Tamil Nadu
+- Course : Full Stack Javascript
+- Project for the iNeuron Hackathon FSJS Batch July 29 - Aug 04.
---
+++ script.js
@@ -0,0 +1,49 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction2();
+  scrollFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction2() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
